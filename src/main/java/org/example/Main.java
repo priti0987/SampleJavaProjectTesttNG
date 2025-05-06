8589b999ab1e446989a097eb772c38c4d65dc405
priti <pritibhushanf@gmail.com>
2025-05-06 17:38:06 +0530

    6may

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello World..!");
+        System.setProperty("webdriver.com.driver","C:\\driver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://selectorshub.com/xpath-practice-page/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+//        driver.close();
     }
 }
